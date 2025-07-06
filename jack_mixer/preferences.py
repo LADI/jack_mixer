5cdbf68ac335091533d6e3baee0199ed010c2f31
Nedko Arnaudov <nedko@nedk.org>
2025-07-06 22:12:20 +0300

    Use SPDX license convention in most source files

--- jack_mixer/preferences.py
+++ jack_mixer/preferences.py
@@ -1,20 +1,8 @@
 # This file is part of jack_mixer
 #
-# Copyright (C) 2006-2025 Nedko Arnaudov
-# Copyright (C) 2009 Frederic Peters <fpeters@0d.be>
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; version 2 of the License
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+# SPDX-FileCopyrightText: Copyright (C) 2006-2025 Nedko Arnaudov
+# SPDX-FileCopyrightText: Copyright (C) 2009 Frederic Peters <fpeters@0d.be>
+# SPDX-License-Identifier: GPL-2.0-only
 
 from os.path import expanduser, isdir
 
